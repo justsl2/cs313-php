3b56f61405ede3c25b05fd0d59007d1aefca25b3
Justin Hurst <jhurst@hurst.local>
2018-09-26 20:49:03 -0500

    Update All

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@
         <div class="logo"><a href="..">Justin's<br>Site</a></div>
         <div class="sideLinks">     
         <a href="..">Home</a> 
-        <a href="assignments.html">Assignments</a> 
+        <a href="assignments.php">Assignments</a> 
         </div>
         <div class="footer">
             &copy; <?php echo date("Y");?>
