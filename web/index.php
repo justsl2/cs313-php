8fd590e00a1431514b92a2b5711cfb6f2a668320
Justin Hurst <jhurst@hurst.local>
2018-09-26 20:11:23 -0500

    Week02 - Prove Files

--- web/index.php
+++ web/index.php
@@ -6,15 +6,9 @@
 
 <link rel="stylesheet" href="mystyle.css">
 <?php
-echo "
-<html>
-    <head>
-        <title>Week11 : Problem 1</title>
-    </head>
-    <body>
-        <p>Hello World</p>
-    </body>
-</html>";
+echo $now = new DateTime();
+echo $now->format('Y-m-d H:i:s');    // MySQL datetime format
+echo $now->getTimestamp();           // Unix Timestamp -- Since PHP 5.
 ?>
 <body>
 
