0240f95b1a4c37c5a8fe25de8ae342f22ce5b2b8
Justin Hurst <jhurst@hurst.local>
2018-10-03 21:23:40 -0500

    Week02 Files

--- web/week03/teach/ta03submit.php
+++ web/week03/teach/ta03submit.php
@@ -10,7 +10,7 @@
 <body>
     <h1>Your Info</h1>
     <p> Name:       <?php echo $_POST["name"]; ?> <br> 
-        Email:      <?php echo "<a href=" . $_POST["email"].">".$_POST["email"]."</a>"; ?><br> 
+        Email:      <?php echo "<a href=mailto:" . $_POST["email"].">".$_POST["email"]."</a>"; ?><br> 
         Major:      <?php echo $_POST["Major"]; ?> <br> 
         Comments:   <?php echo $_POST["comments"]; ?> <br> 
         You have visited: <br>
