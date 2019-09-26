83f07aab3a39268e4d3227d94c08133ea65519ac
Justin Hurst <jhurst@gmail.com>
2019-09-25 20:35:14 -0500

    a

---
+++ web/week02/javascript.js
@@ -0,0 +1,17 @@
+
+function clickMe() {
+	alert("Clicked!");
+}
+
+function changeColor() {
+	var textbox_id = "txtColor";
+	var textbox = document.getElementById(textbox_id);
+
+	var div_id = "div01";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var color = textbox.value;
+	div.style.backgroundColor = color;
+
+}
