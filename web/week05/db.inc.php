b68975b131a2a7064f490d0e47776770471eb6dd
Justin Hurst <jhurst@gmail.com>
2019-10-16 17:44:58 -0500

    Update Files

--- web/week05/db.inc.php
+++ web/week05/db.inc.php
@@ -8,7 +8,7 @@ $dbUrl = getenv('DATABASE_URL');
 
 if (empty($dbUrl)) {
  // example localhost configuration URL with postgres username and a database called cs313db
- $dbUrl = "postgres://postgres:hurst77$@localhost:5432/postgres";
+ $dbUrl = "postgres://postgres:default@localhost:5432/postgres";
 }
 
 $dbopts = parse_url($dbUrl);
@@ -21,7 +21,7 @@ $dbUser = $dbopts["user"];
 $dbPassword = $dbopts["pass"];
 $dbName = ltrim($dbopts["path"],'/');
 
-print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName;pass=$dbPassword;user=$dbUser</p>\n\n";
 
 try {
  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
--- web/week05/index.php
+++ web/week05/index.php
@@ -1,10 +1,3 @@
 <?php
 include 'db.inc.php';
-
-foreach ($db->query('SELECT username, password FROM note_user') as $row)
-{
-  echo 'user: ' . $row['username'];
-  echo ' password: ' . $row['password'];
-  echo '<br/>';
-}
 ?>
