6ec4362bff752c6bd87baed64b907318ab1f32f9
Justin Hurst <jhurst@gmail.com>
2019-10-16 18:16:02 -0500

    Update Files

--- web/week05/index.php
+++ web/week05/index.php
@@ -33,7 +33,7 @@ $db = get_db();
 // Notice that we avoid using "SELECT *" here. This is considered
 // good practice so we don't inadvertently bring back data we don't
 // want, especially if the database changes later.
-$statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
+$statement = $db->prepare("SELECT book, chapter, verse, content FROM scriptures");
 $statement->execute();
 // Go through each result
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
