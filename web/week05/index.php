77b1251de5a5555feacc95100cafbf87eb887ba2
Justin Hurst <jhurst@gmail.com>
2019-10-15 21:32:20 -0500

    Update Files

--- web/week05/index.php
+++ web/week05/index.php
@@ -1,25 +1,5 @@
 <?php $radio = array("Computer Science", "Web Design and Development", "Computer Information Technology", "Computer Engineering"); 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
 
 foreach ($db->query('SELECT username, password FROM note_user') as $row)
 {
