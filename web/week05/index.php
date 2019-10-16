7ad5fbc73f5dc9e5c44d1e208334c6bca1866e29
Justin Hurst <jhurst@gmail.com>
2019-10-15 22:03:47 -0500

    Update Files

--- web/week05/index.php
+++ web/week05/index.php
@@ -1,57 +1,10 @@
-<html>
+<?php
+include 'db.inc.php';
 
-<head>
-    <title>Playing with PHP and Postgres</title>
-
-</head>
-
-<body>
-
-    <?php $radio = array("Computer Science", "Web Design and Development", "Computer Information Technology", "Computer Engineering"); 
-        try
-        {
-        $dbUrl = getenv('DATABASE_URL');
-
-        $dbOpts = parse_url($dbUrl);
-
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
-
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        catch (PDOException $ex)
-        {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-        }
-        ECHO 'TEST 1';
-        echo '<br/>';
-        foreach ($db->query('SELECT username, password FROM note_user') as $row)
-        {
-        echo 'user: ' . $row['username'];
-        echo ' password: ' . $row['password'];
-        echo '<br/>';
-        }
-        ECHO 'TEST 2';
-        echo '<br/>';
-        $statement = $db->query('SELECT username, password FROM note_user');
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-        {
-        echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
-        }
-        ECHO 'TEST 3';
-        echo '<br/>';
-        $statement = $db->query('SELECT username, password FROM note_user');
-        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-        ECHO $results;
-        echo '<br/>';
-    ?>
-
-</body>
-
-</html>
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
+?>
