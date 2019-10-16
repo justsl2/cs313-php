e7362869c088b2a3a55288db37eed640c718a0f7
Justin Hurst <jhurst@gmail.com>
2019-10-16 18:14:43 -0500

    Update Files

--- web/week05/index.php
+++ web/week05/index.php
@@ -1,3 +1,30 @@
+ 
 <?php
-include 'db.inc.php';
+/**********************************************************
+* File: viewScriptures.php
+* Author: Br. Burton
+* 
+* Description: This file shows an example of how to query a
+*   PostgreSQL database from PHP.
+***********************************************************/
+require "db.inc.php";
+$db = get_db();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Scripture List</title>
+</head>
+
+<body>
+<div>
+
+<h1>Scripture Resources</h1>
+
+
+
+
+</div>
+
+</body>
+</html>
