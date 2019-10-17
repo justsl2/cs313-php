cb9b1d2adb9a075542da99ff20696ef781cf0ff6
Justin Hurst <jhurst@gmail.com>
2019-10-16 19:33:02 -0500

    Update Files

--- web/week05/index.php
+++ web/week05/index.php
@@ -6,8 +6,10 @@
 * Description: This file shows an example of how to query a
 *   PostgreSQL database from PHP.
 ***********************************************************/
-require "db.inc.php";
+
+require "dbConnect.php";
 $db = get_db();
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,39 +23,29 @@ $db = get_db();
 <h1>Scripture Resources</h1>
 
 <?php
-// In this example, for simplicity, the query is executed
-// right here and the data echoed out as we iterate the query.
-// You could imagine that in a more involved application, we
-// would likely query the database in a completely separate file / function
-// and build a list of objects that held the components of each
-// scripture. Then, here on the page, we could simply call that 
-// function, and iterate through the list that was returned and
-// print each component.
-// First, prepare the statement
-// Notice that we avoid using "SELECT *" here. This is considered
-// good practice so we don't inadvertently bring back data we don't
-// want, especially if the database changes later.
-$statement = $db->prepar('select * from Scriptures Where book=:bookName');
-$statement->bindValue(':bookName', $_get['bookName'], PDO::PAQRAM_STR);
-$statement->execute();
-$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-// Go through each result
-foreach ($row = $row)
+
+// Search Scriptures from a Book
+$stmt = $db->prepare('select * from Scripture WHERE book=:bookName');
+$stmt->bindValue(':bookName', $_GET['bookName'], PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($rows as $row)
 {
-	// The variable "row" now holds the complete record for that
-	// row, and we can access the different values based on their
-	// name
-	$book = $row['book'];
-	$chapter = $row['chapter'];
-	$verse = $row['verse'];
-	$content = $row['content'];
-	echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+  //<b>John 3:16</b> - "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."
+  echo '<p>';
+  echo '<a href="scripture-details.php?id=' . $row['id'] . '">';
+  echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b>';
+  echo '</a>';
+  echo '</p>';
 }
+
 ?>
+
 <form>
-Book: <input type='text' name="bookName">
+Book: <input type="text" name="bookName">
 <br/>
-<input type = "submit" value="Search">
+<input type="submit" value="Search">
 </form>
 
 
