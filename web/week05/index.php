e9f07d34e5a162cb900e375344b1901b7276ae68
Justin Hurst <jhurst@gmail.com>
2019-10-16 19:52:47 -0500

    Update Files

--- web/week05/index.php
+++ web/week05/index.php
@@ -20,34 +20,32 @@ $db = get_db();
 <body>
 <h1>Scripture Resources</h1>
 
+<form>
+    Book: <input type="text" name="bookName">
+    <br/>
+    <input type="submit" value="Search">
+</form>
+
 <?php
 
 // Search Scriptures from a Book
 
 if (isset($_GET['bookName']))
 {
-$stmt = $db->prepare('select * from Scripture WHERE book=:bookName');
-$stmt->bindValue(':bookName', $_GET['bookName'], PDO::PARAM_STR);
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $db->prepare('select * from Scripture WHERE book=:bookName');
+    $stmt->bindValue(':bookName', $_GET['bookName'], PDO::PARAM_STR);
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($rows as $row)
-{
-  //<b>John 3:16</b> - "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."
-  echo '<p>';
-  echo '<a href="scripture-details.php?id=' . $row['id'] . '">';
-  echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b>';
-  echo '</a>';
-  echo '</p>';
-}
+    foreach ($rows as $row)
+    {
+        echo '<p>';
+        echo '<a href="scripture-details.php?id=' . $row['id'] . '">';
+        echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b>';
+        echo '</a>';
+        echo '</p>';
+    }
 }
 ?>
-
-<form>
-Book: <input type="text" name="bookName">
-<br/>
-<input type="submit" value="Search">
-</form>
-
 </body>
 </html>
