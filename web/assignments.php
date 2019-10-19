60587b649850bfd8ff57a22daeffdc5819e2425f
Justin Hurst <jhurst@gmail.com>
2019-10-19 18:01:33 -0500

    Update Files

--- web/assignments.php
+++ web/assignments.php
@@ -16,7 +16,8 @@
       <!-- Header -->
         <h1 class="header">Assignments</h1>
         <a class="about" href="..\week03\teach\index.php">Week 03 Teach</a><br>      
-        <a class="about" href="..\week03\prove\index.php">Week 03 Prove</a><br>    
+        <a class="about" href="..\week03\prove\index.php">Week 03 Prove</a><br>  
+        <a class="about" href="..\project\index.php">Project - IMS</a><br>  
     </div>
 
 </body>
