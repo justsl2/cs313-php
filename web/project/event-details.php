04e46d7edd59871722b1035117179676133b380b
Justin Hurst <jhurst@gmail.com>
2019-10-19 22:50:00 -0500

    Update Files

--- web/project/event-details.php
+++ web/project/event-details.php
@@ -194,7 +194,8 @@
         echo '<b>Date Reported:</b>  ' . $dateReported->format('M d, Y').'<br>';
         echo '<b>Date Entered:</b>  ' . $dateEntered->format('M d, Y').'<br>';
         echo '<b>Within Reporting Boundaries?:</b>  ' . var_export($row['reporting_boundary'], True) . '<br>';
-
+        echo <br>;
+        echo <br>;
         //Consequence Type
         $sql = "select consequence_type_label from consequence_types right join events on consequence_types.consequence_type_id = events.consequence_type_id WHERE event_id=:event_id";
         $stmt = $db->prepare($sql);
