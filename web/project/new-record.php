0107d4046d2e350faf7a7bcc2b17db20341a6997
Justin Hurst <jhurst@gmail.com>
2019-10-26 12:40:52 -0500

    Update Files

--- web/project/new-record.php
+++ web/project/new-record.php
@@ -12,6 +12,7 @@ $stmt = $db->prepare('INSERT INTO events (date_occurred, description_short, equi
 $stmt->bindValue(':dateOccurred',$dateOccurred); 
 $stmt->bindValue(':shortDescription',$shortDescription); 
 $stmt->bindValue(':equipmentID',$equipmentID); 
+echo $stmt . '<br>';
 $stmt->execute();
 
     $stmt = $db->prepare('SELECT event_id, date_occurred, description_short, equipment_id from events');
@@ -19,7 +20,7 @@ $stmt->execute();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 	{
         echo '<p>';
-        echo 'Event ID = ' . $row['event_id'] . '<br>';
+        //echo 'Event ID = ' . $row['event_id'] . '<br>';
         //echo 'Date Event Occurred = ' . $row['date_occurred'] . '<br>';
         //echo 'Short Description = ' . $row['description_short'] . '<br>';
         //echo 'Equipment ID = '. $row['equipment_id'] . '<br>';
