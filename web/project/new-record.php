ac7539dcd278ff5cdda7d48ce24e678c82ee9797
Justin Hurst <jhurst@gmail.com>
2019-10-26 12:08:19 -0500

    Update Files

--- web/project/new-record.php
+++ web/project/new-record.php
@@ -2,23 +2,21 @@
 require "dbConnect.php";
 $db = get_db();
 $shortDescription = $_POST['shortDescription'];
-$equipment = $_POST['equipment'];
+$equipmentID = $_POST['equipmentID'];
 
 
-$stmt = $db->prepare('INSERT INTO events (description_short, equipment) VALUES (:shortDescription, :equipment)');
+$stmt = $db->prepare('INSERT INTO events (description_short, equipment_id) VALUES (:shortDescription, :equipmentID)');
 $stmt->bindValue(':shortDescription',$shortDescription); 
-$stmt->bindValue(':equipment',$equipment); 
+$stmt->bindValue(':equipmentID',$equipmentID); 
 $stmt->execute();
 
-$eventID = $db->lastInsertId("events_event_id_seq");
-
-    $stmt = $db->prepare('SELECT event_id, description_short, equipment from events');
+    $stmt = $db->prepare('SELECT event_id, description_short, equipment_id from events');
     $stmt->execute();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 	{
 		echo '<p>';
 		echo '<strong>' . $row['event_id'] . ' ' . $row['description_short'] . ':';
-		echo $row['equipment'] . '</strong>';
+		echo $row['equipment_id'] . '</strong>';
 		echo '<br />';
 		echo '</p>';
 	}
