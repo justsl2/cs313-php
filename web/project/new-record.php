6295cbe433ed76ce9691690d1635f5ba8c77d524
Justin Hurst <jhurst@gmail.com>
2019-10-26 13:12:15 -0500

    Update Files

--- web/project/new-record.php
+++ web/project/new-record.php
@@ -1,6 +1,24 @@
 <?php
 require "dbConnect.php";
 $db = get_db();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Incident Management System</title>
+        <link rel="stylesheet" type="text/css" href="mystyles.css">
+    </head>
+
+    <body>
+    <h1>Incident Management System</h1>
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a class="active" href="new.php">Create New Event</a>
+        <a class="active" href="search.php">Search</a>
+    </div>
+    <h2>Record Entered Successfully</h2>
+<?php
 $dateOccurred = $_POST['dateOccurred'];
 $shortDescription = $_POST['shortDescription'];
 $longDescription = $_POST['longDescription'];
