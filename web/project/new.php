aa4f9f4bfbfea422eeef8d12411f61d532dee369
Justin Hurst <jhurst@gmail.com>
2019-10-26 13:53:48 -0500

    Update Files

--- web/project/new.php
+++ web/project/new.php
@@ -17,17 +17,18 @@
         <a class="active" href="new.php">Create New Event</a>
         <a class="active" href="search.php">Search</a>
     </div>
-    <h2>New</h2>
+    <h2>New Event Record</h2>
     <form method="post" action="new-record.php">
    
+    <h3>Event Details:</h3>
     Date Event Occurred: 
     <input required type="date" name="dateOccurred"><br/>
     
     Short Description: 
-    <input required type="text" rows="2" cols="100" name="shortDescription"><br/>
+    <input required type="textarea" rows="2" cols="100" name="shortDescription"><br/>
 
     Detailed Description: 
-    <input required type="text" rows="4" cols="100" name="longDescription"><br/>
+    <input required type="textarea" rows="4" cols="100" name="longDescription"><br/>
 
     Site: 
         <select required name="siteID">
@@ -81,6 +82,25 @@
         </select>
         <br/>
     Probable Severity of Event: 
+        <select required name="severityID_Prob">
+        <option value="" selected disabled hidden></option>
+        <?php
+            $stmt = $db->prepare('select * from severities');
+            $stmt->execute();
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($rows as $row)
+            {
+                $severity = $row['severity_label'];
+                $severityID = $row['severity_id'];
+                echo '<option value="'.$severityID.'">'.$severity.'</option>';
+                echo '<br>';
+            }
+        ?>
+        </select>
+        <br/>
+    Temperature: 
+    <input required type="text" rows="4" cols="100" name="longDescription"><br/>
+    Unit of Measure: 
         <select required name="severityID_Prob">
         <option value="" selected disabled hidden></option>
         <?php
