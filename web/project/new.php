82e8989eb3e2520bec8b1dd5f8ba7be800497d5c
Justin Hurst <jhurst@gmail.com>
2019-10-26 13:14:05 -0500

    Update Files

--- web/project/new.php
+++ web/project/new.php
@@ -19,14 +19,15 @@
     </div>
     <h2>New</h2>
     <form method="post" action="new-record.php">
+   
     Date Event Occurred: 
-    <input type="date" name="dateOccurred"><br/>
+    <input required type="date" name="dateOccurred"><br/>
     
     Short Description: 
-    <input type="text" rows="2" cols="100" name="shortDescription"><br/>
+    <input required type="text" rows="2" cols="100" name="shortDescription"><br/>
 
     Detailed Description: 
-    <input type="text" rows="4" cols="100" name="longDescription"><br/>
+    <input required type="text" rows="4" cols="100" name="longDescription"><br/>
 
     Equipment: 
         <select required name="equipmentID">
