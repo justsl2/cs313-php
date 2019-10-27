82b7d8a9796b0556fd8b72cc2ce2b2307b5f7ede
Justin Hurst <jhurst@gmail.com>
2019-10-26 21:40:12 -0500

    Update Files

--- web/project/edit-record.php
+++ web/project/edit-record.php
@@ -34,8 +34,8 @@ $db = get_db();
         echo '<b>Event ID: </b> <br/>'.$row['event_id'].'<br>';
         echo '<b>Date Event Occurred: </b> <br/><input required type="date" name="dateOccurred" style="width:200px" value='.$row['date_occurred'].'><br>';
         echo '<b>Date Event Reported: </b> <br/><input required type="date" name="dateReported" style="width:200px" value='.$row['date_reported'].'><br>';
-        echo '<b>Short Description: </b> <br/><textarea required name="shortDescription" rows="1" cols="70" value='.$row['description_short'].'></textarea><br>';
-        echo 'Detailed Description: '.$row['description_long'].'<br>';
+        echo '<b>Short Description: </b> <br/><textarea required name="shortDescription" rows="1" cols="70">'.$row['description_short'].</textarea><br>';
+        
         }
 
     ?>
