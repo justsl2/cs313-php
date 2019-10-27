94aa4a93d4c9632a4cee30a8a347c5564e5943c3
Justin Hurst <jhurst@gmail.com>
2019-10-26 23:28:57 -0500

    Update Files

--- web/project/edit-record-confirmation.php
+++ web/project/edit-record-confirmation.php
@@ -47,8 +47,7 @@ $consequenceID = $_POST['consequenceID'];
 
 
 $sql = "UPDATE public.events 
-        SET severity_probable_id=".$severityID_Prob.
-        " WHERE event_id=". $EventID;
+        SET severity_probable_id=".$severityID_Prob." WHERE event_id=". $EventID;
 echo $sql;
 $stmt = $db->prepare($sql);
 
