4a2c89876eb1463ef2992c87ccafe832d5337223
Justin Hurst <jhurst@gmail.com>
2019-11-02 23:32:48 -0500

    Update Files

--- web/project/edit-record-confirmation.php
+++ web/project/edit-record-confirmation.php
@@ -80,32 +80,32 @@ $injuryPrimaryBodyPartID = $_POST['injuryPrimaryBodyPartID'];
 $companyNameID = $_POST['companyNameID'];
 $injuryLostDays = $_POST['injuryLostDays'];
 $injuryLostDaysStartDate = $_POST['injuryLostDaysStartDate'];
-echo 'injuryID:  '.$injuryID.'<br/>';
-echo 'injuryDescription:  '.$injuryDescription.'<br/>';
-echo 'medClassID:  '.$medClassID.'<br/>';
-echo 'workRelated:  '.$workRelated.'<br/>';
-echo 'personnelTypeID:  '.$personnelTypeID.'<br/>';
-echo 'injuryNatureID:  '.$injuryNatureID.'<br/>';
-echo 'injuryPrimaryBodyPartID:  '.$injuryPrimaryBodyPartID.'<br/>';
-echo 'companyNameID:  '.$companyNameID.'<br/>';
-echo 'injuryLostDays:  '.$injuryLostDays.'<br/>';
-echo 'injuryLostDaysStartDate:  '.$injuryLostDaysStartDate.'<br/>';
 
 
-$sql = "UPDATE public.injuries SET 
+
+$injsql = "UPDATE public.injuries SET 
             injury_description='".$injuryDescription."',
             medical_classification_id=".$medClassID.",
             work_related='".$workRelated."',
             personnel_type_id=".$personnelTypeID.",
             injury_nature_id=".$injuryNatureID.",
             injury_primary_body_part_id=".$injuryPrimaryBodyPartID.",            
-            injury_lost_days='".$injuryLostDays."',
+            injury_lost_days=".$injuryLostDays.",
             injury_lost_days_start_date='".$injuryLostDaysStartDate."',
             company_name_id=".$companyNameID.
             " WHERE injury_id=". $injuryID;
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-
+    $injstmt = $db->prepare($injsql);
+    $injstmt->execute();
+    echo 'injuryID:  '.$injuryID.'<br/>';
+    echo 'injuryDescription:  '.$injuryDescription.'<br/>';
+    echo 'medClassID:  '.$medClassID.'<br/>';
+    echo 'workRelated:  '.$workRelated.'<br/>';
+    echo 'personnelTypeID:  '.$personnelTypeID.'<br/>';
+    echo 'injuryNatureID:  '.$injuryNatureID.'<br/>';
+    echo 'injuryPrimaryBodyPartID:  '.$injuryPrimaryBodyPartID.'<br/>';
+    echo 'companyNameID:  '.$companyNameID.'<br/>';
+    echo 'injuryLostDays:  '.$injuryLostDays.'<br/>';
+    echo 'injuryLostDaysStartDate:  '.$injuryLostDaysStartDate.'<br/>';
     echo 'Event ID: '.$EventID.' edited successfully';
 
 
