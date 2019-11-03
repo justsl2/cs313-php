aa3b45aea5b707fee7d1e04f61624a4628fcb775
Justin Hurst <jhurst@gmail.com>
2019-11-02 23:27:36 -0500

    Update Files

--- web/project/edit-record-confirmation.php
+++ web/project/edit-record-confirmation.php
@@ -80,6 +80,16 @@ $injuryPrimaryBodyPartID = $_POST['injuryPrimaryBodyPartID'];
 $companyNameID = $_POST['companyNameID'];
 $injuryLostDays = $_POST['injuryLostDays'];
 $injuryLostDaysStartDate = $_POST['injuryLostDaysStartDate'];
+echo 'injuryID:  '.$injuryID;
+echo 'injuryDescription:  '.$injuryDescription;
+echo 'medClassID:  '.$medClassID;
+echo 'workRelated:  '.$workRelated;
+echo 'personnelTypeID:  '.$personnelTypeID;
+echo 'injuryNatureID:  '.$injuryNatureID;
+echo 'injuryPrimaryBodyPartID:  '.$injuryPrimaryBodyPartID;
+echo 'companyNameID:  '.$companyNameID;
+echo 'injuryLostDays:  '.$injuryLostDays;
+echo 'injuryLostDaysStartDate:  '.$injuryLostDaysStartDate;
 
 
 $sql = "UPDATE public.injuries SET 
