53432e11bee320fe0a2d455c1b9f6f531efeffca
Justin Hurst <jhurst@gmail.com>
2019-11-02 20:55:52 -0500

    Update Files

--- web/project/edit-record.php
+++ web/project/edit-record.php
@@ -62,10 +62,10 @@ $db = get_db();
                     
 
 
-                    $query = 'SELECT severity_id FROM severities WHERE severity_id='.$row['severity_actual_id'];
+                    $query = "SELECT severity_id FROM severities WHERE severity_id=".$row['severity_actual_id'];
                     $statement = $db->prepare($query);
                     $statement->execute();
-                    $result = $statement->fetch();
+                    $result = $statement->get_result()->fetcth_object()->severity_id;
                     echo '<option value="'.$result.'">'.$result.'</option>';
 
                     $stmt = $db->prepare('select * from severities');
