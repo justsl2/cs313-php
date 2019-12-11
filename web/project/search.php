cbfb6f3393eb7cd5376f59f0deb9a950e1050733
Justin Hurst <jhurst@gmail.com>
2019-12-10 18:14:09 -0600

    Update Files

--- web/project/search.php
+++ web/project/search.php
@@ -19,7 +19,9 @@
     </div>
     <h2>Search</h2>
     <form>
-        Event ID: <input type="number" name="eventID" value=1001 >
+        Search by Event ID: <input type="number" name="eventID" value=1001 >
+        OR
+        Search by Date Range: <input type="date" name="startDate"><input type="date" name="endDate">
         <input type="submit" value="Search Event ID" class="button">
     </form>
     
